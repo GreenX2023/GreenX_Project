24c853b82f57da814787edb2b44a14e358c12fa7
austin <austin316dsouza@gmail.com>
2022-10-13 12:02:54 +0530

    added .env and launch.json

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,25 +1,12 @@
 import mongoose from 'mongoose'
+import dotenv from "dotenv";
+dotenv.config();
+const dbLink:string = process.env.ATLAS_MONGO_URL!
 
 // mongodb+srv://ozzy:ozzy@ozzy-cluster.llaahyp.mongodb.net/green?retryWrites=true&w=majority
 
-// exports.mongoLocal = async () =>{
-//     try{
-//         mongoose.connect('mongodb+srv://ozzy:ozzy@ozzy-cluster.llaahyp.mongodb.net/?retryWrites=true&w=majority',{
-//             useNewUrlParser:true,
-//             useUnifiedTopology:true,
-            
-//         });
-//         console.log("DB connected!");
-        
-
-//     }catch(e){
-//         console.log(e);
-//         process.exit(1);
-//     }
-// }
-
 exports.mongoLocal=()=>{
-    mongoose.connect('mongodb://ozzy:ozzy@ac-qnff1hf-shard-00-00.llaahyp.mongodb.net:27017,ac-qnff1hf-shard-00-01.llaahyp.mongodb.net:27017,ac-qnff1hf-shard-00-02.llaahyp.mongodb.net:27017/greenx?ssl=true&replicaSet=atlas-7lxkwp-shard-0&authSource=admin&retryWrites=true&w=majority').then(()=>{
+    mongoose.connect(dbLink).then(()=>{
         console.log(`Atlas DB CONNECTED`);
     }).catch((err)=>{
         console.log(err);
