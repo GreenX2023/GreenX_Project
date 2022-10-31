2588f098888f3ee9aff6a99facff0c830d9df242
Wiznidhi <wiznidhi@gmail.com>
2022-10-31 14:36:31 +0530

    remade pool

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="openingDiv">
-          <a href="https://authdemoo.auth.us-east-2.amazoncognito.com/login?client_id=9guiqeob9jvn5gjb04oou9dnk&response_type=token&scope=email+openid+phone+profile&redirect_uri=https://www.ex.com/cb">LOGIN</a>
+          <a href="https://demoapppgreen.auth.us-east-1.amazoncognito.com/login?client_id=7e1ok10autrmdlh74bvgkjgrr6&response_type=code&scope=email+openid&redirect_uri=http://localhost:3000">LOGIN</a>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to GreenX</p>
